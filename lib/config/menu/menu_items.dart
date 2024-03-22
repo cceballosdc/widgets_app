27b50cd62ac3ab80d43dd1b4a87f69021b1ae341
Desarrollo_DB <ceballosdc@gmail.com>
2024-03-22 16:08:02 -0600

    uid screen terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -39,5 +39,10 @@ const appMenuItem = <MenuItem>[
     title: 'Animated Container', 
     subTitle: 'Stateful Widget animado', 
     link: '/animated', 
-    icon: Icons.check_box_outline_blank_rounded),    
+    icon: Icons.check_box_outline_blank_rounded),  
+  MenuItem(
+    title: 'UI Controls + Titles', 
+    subTitle: 'Una serie de controles de flutter', 
+    link: '/ui-controls', 
+    icon: Icons.car_rental_rounded),   
 ];
